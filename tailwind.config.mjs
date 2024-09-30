c4cacddfafc7cd8d90ad1405194ef037d66b5a0f
Jakob <4ipset@gmail.com>
2024-09-30 22:47:29 +0400

    d

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,8 +5,8 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     screens: {
-      'sm': '576px',
-      // => @media (min-width: 576px) { ... }
+      'sm': '720px',
+      // => @media (min-width: 720px) { ... }
 
       'md': '960px',
       // => @media (min-width: 960px) { ... }
