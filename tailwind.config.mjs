4ead064df26efb72d1fa18a57bb12a4a4c90ab07
Jakob <4ipset@gmail.com>
2024-09-30 22:44:28 +0400

    fff

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,16 @@ export default {
   darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    screens: {
+      'sm': '576px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '960px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '1440px',
+      // => @media (min-width: 1440px) { ... }
+    },
     extend: {
       fontFamily: {
         sans: ["Geist Sans", ...defaultTheme.fontFamily.sans],
@@ -13,3 +23,4 @@ export default {
   },
   plugins: [require("@tailwindcss/typography")],
 };
+
