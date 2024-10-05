26799899fdcbee1a55d9a6687e2b5fa889061143
Jakob <4ipset@gmail.com>
2024-10-05 19:13:07 +0400

    24

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,9 +4,16 @@ const blog = defineCollection({
   type: "content",
   schema: z.object({
     title: z.string(),
-    description: z.string(),
-    date: z.coerce.date(),
-    draft: z.boolean().optional(),
+    description: z.string().optional().nullable(),
+    date: z.date(),
+    tags: z.array(z.string()).or(z.string()).optional().nullable(),
+    category: z.array(z.string()).or(z.string()).optional().nullable(),
+    sticky: z.number().default(0).nullable(),
+    mathjax: z.boolean().default(false).nullable(),
+    draft: z.boolean().default(false).nullable(),
+    toc: z.boolean().default(true).nullable(),
+    donate: z.boolean().default(true).nullable(),
+    comment: z.boolean().default(true).nullable(),
   }),
 });
 
